ffc524f51d099d4013cc07d051ff35b2d4b7115b
Ravil <ravilgab@yahoo.com>
2022-11-17 00:41:43 +0300

    Project created

---
+++ Homework_2.28_3_RgbSliders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_2.28_3_RgbSliders
+//
+//  Created by Ravil on 17.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
