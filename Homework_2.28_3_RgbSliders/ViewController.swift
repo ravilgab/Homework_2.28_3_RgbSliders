b8eefd67493580d499c56d21574fe624f6c74147
Ravil <ravilgab@yahoo.com>
2022-11-17 19:58:50 +0300

    Homework is done as logic is ready

--- Homework_2.28_3_RgbSliders/ViewController.swift
+++ Homework_2.28_3_RgbSliders/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        colorView.layer.cornerRadius = 16
+        
+        setLabelValue()
+        paintView()
     }
 
-
+    @IBAction func masterSliderMoved(_ sender: UISlider) {
+        if sender == redSlider {
+            redLabel.text = convertToString(from: sender)
+        } else if sender == greenSlider {
+            greenLabel.text = convertToString(from: sender)
+        } else {
+            blueLabel.text = convertToString(from: sender)
+        }
+        
+        paintView()
+    }
+    
+    private func setLabelValue() {
+        redLabel.text = convertToString(from: redSlider)
+        greenLabel.text = convertToString(from: greenSlider)
+        blueLabel.text = convertToString(from: blueSlider)
+    }
+    
+    private func paintView() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func convertToString(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
 }
 
